e6e08f3075f65738e856616985be2db3aae1d3b1
george.5 <george.5@kakaocorp.com>
2022-09-06 14:45:07 +0900

    [페어 프로그래밍으로 인한 커밋]
    1. 전체적인 프로젝트 설계 구조 잡기
    2. 필요한 클래스들 생각

--- src/main/java/lotto/Main.java
+++ src/main/java/lotto/Main.java
@@ -6,6 +6,22 @@ public class Main {
         return "Hello";
     }
     public static void main(String[] args) {
+        // 1. money -> 14000원
+
+        // 2. money -> 해당하는 티켓 갯수를 구해야한다. => ticketCnt
+
+        // 3. TicketMaker는 객체 -> 자동, 수동 메서드 따로 정의(ticket 갯수)
+
+        // 4. lotto.run(tickets)
+
+        // 5. winningNumbers -> 사용자 입력 당첨 번호들
+
+        // 6. lotto.result(winningNumbers); -> 지금 당장은 Map 충분하지 않을까?
+
+        // result로 받는것도 좋을 듯!
+
+        // 7. 받아서 이율 계산하고 보여주고~
+
         System.out.println(Main.getHello());
     }
 }
---
+++ src/test/java/lotto/TicketBuyerTest.java
@@ -0,0 +1,14 @@
+package lotto;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketBuyerTest {
+    @Test
+    @DisplayName("ticket 구매하기")
+    void buy() {
+
+    }
+}
