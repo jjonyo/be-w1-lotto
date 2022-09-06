14e144ce9e1506dd89eb27783513c20ce9cce59c
jjonyo <jonghyuk96@cau.ac.kr>
2022-09-06 17:06:16 +0900

    FEAT : 로또 클래스 추가 로또의 번호와 당첨 결과를 표현하는 로또 클래스를 추가했습니다.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,17 +1,44 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
   private final List<Integer> numbers;
+  private Rank rank;
 
   public Lotto(List<Integer> numbers) {
+    Collections.sort(numbers);
+
     this.numbers = new ArrayList<>(numbers);
   }
 
   public List<Integer> getNumbers() {
     return numbers;
+  }
+
+  public void calculateRank(Lotto winningLotto) {
+    int countOfMatch = countMatchNumber(winningLotto);
+
+    this.rank = Rank.valueOf(countOfMatch);
+  }
+
+  private int countMatchNumber(Lotto winningLotto) {
+    int countOfMatch = 0;
+
+    for (Integer number : numbers) {
+      if (winningLotto.getNumbers().contains(number)) {
+        countOfMatch += 1;
+      }
+    }
+
+    return countOfMatch;
+  }
+
+
+  public Rank getRank() {
+    return rank;
   }
 
   @Override
