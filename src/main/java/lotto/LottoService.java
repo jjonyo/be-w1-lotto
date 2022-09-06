210b5dc899e098ef676206e2d2e85cc5181999a3
kakao <kakao@kakaoui-MacBookPro.local>
2022-09-06 16:14:05 +0900

    페어프로그래밍 역할 교체

--- src/main/java/lotto/LottoService.java
+++ src/main/java/lotto/LottoService.java
@@ -1,25 +1,32 @@
 package lotto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LottoService {
   private final List<Lotto> lottoList = new ArrayList<>();
+  private static List<Integer> numberList = IntStream.range(1, 46)
+          .boxed()
+          .collect(Collectors.toList());
+
 
   public void generateLottoList(int size) {
     lottoList.clear();
 
     for (int i=0; i<size; i++) {
-      lottoList.add(generateLotto());
+      lottoList.add(generateRandomLotto());
     }
   }
 
   public List<Lotto> getLottoList() {
     return lottoList;
   }
-  private Lotto generateLotto() {
-
-    return null;
+  private Lotto generateRandomLotto() {
+    Collections.shuffle(numberList);
+    return new Lotto(numberList.subList(0, 6));
   }
 
 }
--- src/main/java/lotto/LottoView.java
+++ src/main/java/lotto/LottoView.java
@@ -10,13 +10,16 @@ public class LottoView {
     // 구입금액 입력받는 메소드 호출
     System.out.println("구입 금액을 입력해 주세요.");
     int price = scanner.nextInt();
-    int size = price / 10;
+    int size = price / 1000;
+    System.out.println(size + "개를 구매했습니다.");
 
     // 입력받은 금액으로 로또 생성하기 (lottoService 호출)
     lottoService.generateLottoList(size);
 
     // 구매결과 출력 메소드
-//    lottoService.getLottoList();
+    for (Lotto lotto : lottoService.getLottoList()) {
+      System.out.println(lotto.toString());
+    };
 
     // 당첨번호 입력받는 메소드 호출
     System.out.println("지난 주 당첨 번호를 입력 해 주세요.");
@@ -24,8 +27,12 @@ public class LottoView {
 
     // 당첨번호 파싱하여 integer array 반환하는 메소드
 
+
     // 당첨 계산 (lottoService 호출)
 
+
     // 당첨 통계 출력
+
+
   }
 }
