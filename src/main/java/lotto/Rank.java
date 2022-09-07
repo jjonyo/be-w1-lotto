59bc0e0368c58d6a9487ff69f9e5b53e9dba9683
jjonyo <jonghyuk96@cau.ac.kr>
2022-09-07 16:37:11 +0900

    FEAT : Rank Enum NONE값 추가 Rank Enum이 제대로 계산되었는지 테스트를 위해 NONE값 추가

--- src/main/java/lotto/Rank.java
+++ src/main/java/lotto/Rank.java
@@ -5,7 +5,8 @@ public enum Rank {
   SECOND(5, 30000000),
   THIRD(5, 1500000),
   FOURTH(4, 50000),
-  FIFTH(3, 5000);
+  FIFTH(3, 5000),
+  NONE(-1, 0);
 
   private int countOfMatch;
   private int winningMoney;
@@ -34,6 +35,6 @@ public enum Rank {
       }
     }
 
-    return null;
+    return NONE;
   }
 }
